992d79895fe5270264bb7f6a855cc2bd50c7cf1a
Pierre Charrat <pierre.charrat@gmail.com>
2016-05-02 18:46:43 +0200

    fin BD + debut produit.php

---
+++ Produit.php
@@ -0,0 +1,24 @@
+<?php
+Class Produit{
+public $id;
+public $code;
+public $marque;
+public $urlImage;
+public $description;
+public $prix;
+public $poids;
+public $note;
+
+function __construct($array){
+  $this->id = $array[0];
+  $this->code =$array[1];
+  $this->marque= $array[2];
+  $this->urlImage = $array[3];
+  $this->description = $array[4];
+  $this->prix = $array[5];
+  $this->poids = $array[6];
+  $this->note = $array[7];
+
+}
+
+ ?>
--- tesBD.php
+++ tesBD.php
@@ -1,15 +1,22 @@
 <?php
-function get($id){
-  $req = "SELECT * FROM music WHERE ID='$id'";
-  $sth = $this->db->query($req);
-  $result = $sth->fetchAll();
-  var_dump($result);
-  $music = new Music($result[0]);
-  /*var_dump($music);
-  exit(0);*/
-  return 1;
+Class MusicDAO{
+  private $db;
+
+  function get($id){
+    $req = "SELECT * FROM music WHERE ID='$id'";
+    $sth = $this->db->query($req);
+    $result = $sth->fetchAll();
+    //var_dump($result);
+    $music = new Music($result[0]);
+    /*var_dump($music);
+    exit(0);*/
+    return $music;
+  }
+  function __construct(){
+    $this->db = new PDO('sqlite:music.bd');
+  }
 }
 
-
-get(1);
+$tes = new MusicDAO();
+tes.get(1);
 ?>
