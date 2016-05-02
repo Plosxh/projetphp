f03c8e649aa65c5dfb84b2943d953caaa88b98fa
Pierre Charrat <pierre.charrat@gmail.com>
2016-05-02 17:47:29 +0200

    commit produit class

---
+++ drop.sql
@@ -0,0 +1,9 @@
+DROP TABLE panier;
+
+DROP TABLE commande;
+
+DROP TABLE stock;
+
+DROP TABLE produit;
+
+DROP TABLE utilisateur;
---
+++ insert.sql
@@ -0,0 +1,13 @@
+set datestyle to european;
+
+
+
+insert into produit(id,code,marque,urlimage,description,prix,poids,note) values (1,'chaussbask','adidas','URL','chaussure basket',99.9,780,0);
+insert into produit(id,code,marque,urlimage,description,prix,poids,note) values (2,'chaussville','adidas','URL','chaussure de ville',99.9,780,0);
+insert into produit(id,code,marque,urlimage,description,prix,poids,note) values (3,'chausstong','adidas','URL','chaussure tong',99.9,780,0);
+insert into produit(id,code,marque,urlimage,description,prix,poids,note) values (4,'chausssandale','adidas','URL','chaussure sandale',99.9,780,0);
+insert into produit(id,code,marque,urlimage,description,prix,poids,note) values (5,'vetpant','adidas','URL','pantalon',99.9,780,0);
+insert into produit(id,code,marque,urlimage,description,prix,poids,note) values (6,'vetrobe','adidas','URL','robe',99.9,780,0);
+insert into produit(id,code,marque,urlimage,description,prix,poids,note) values (7,'vetshirt','adidas','URL','T-shirt',99.9,780,0);
+insert into produit(id,code,marque,urlimage,description,prix,poids,note) values (8,'vetpull','adidas','URL','Pull',99.9,780,0);
+insert into produit(id,code,marque,urlimage,description,prix,poids,note) values (9,'vetVvest','adidas','URL','Veste',99.9,780,0);
